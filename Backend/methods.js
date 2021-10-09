14c2745221e25e0e2f2f05a5bd53ceb5652f9f2c
Chetas Sharma <sharmachetas.111@gmail.com>
2021-10-09 18:46:55 +0530

    creating endpoints of API

---
+++ Backend/methods.js
@@ -0,0 +1,48 @@
+const axios = require("axios").default
+const API = require("./api");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccount.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+
+const quoteURL = "https://finance.yahoo.com/quote"
+
+const getTopGainerStocks =  async (amt) => {
+    const curr = amt / 72;
+    const response = await axios.get(API.stocks);
+    const data = response.data?.slice(0, 5).map((stock) => {
+        return {
+            ...stock,
+            url: `${quoteURL}/${stock.ticker}`,
+            amount: curr / Number(stock.price)
+        };
+    })
+    return data;
+}
+
+module.exports = {
+    getUserData : async (id) => {
+        const userRef = db.collection('users').doc(id);
+        const getUser = async () => {
+            const doc = await userRef.get();
+            if (!doc.exists) {
+                console.log('No such user!');
+                return NaN;
+            } else {
+                return doc.data();
+            }
+        }
+
+        const userData = await getUser();
+        // const {amt} = userData;
+        const amt = 300;
+        if(amt <= 0){
+            return NaN;
+        }
+        return getTopGainerStocks(amt);
+    }
+
+};
--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,8 @@
 require('dotenv').config();
 const express = require("express");
-const axios = require("axios");
 const cors = require("cors");
-const API  = require("./api");
-const admin = require("firebase-admin")
 const app = express();
-
-const serviceAccount = require("./serviceAccount.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
-
-const db = admin.firestore();
+const {getUserData} = require('./methods')
 
 const PORT = process.env.PORT || 5000;
 
@@ -23,8 +13,12 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
-// console.log(API.stocks);
-
-console.log(db);
+app.get("/investments", async (req,res) => {
+    const userID = req.query.id;
+    // console.log(userID);
+    const data = await getUserData(userID);
+    console.log(data);
+    res.send(data);
+});
 
 app.listen(PORT, () => console.log("Server is running..."));
